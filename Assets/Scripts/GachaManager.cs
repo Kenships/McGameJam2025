55f18d180e9a2c11ffc551e319fd5eb23769f158
heinz-ketchup <gawainxiong@gmail.com>
2025-01-11 11:07:58 -0500

    maze working sortav

---
+++ Assets/Scripts/GachaManager.cs
@@ -0,0 +1,32 @@
+// using UnityEngine;
+//
+// public class GachaManager : MonoBehaviour
+// {
+//     public class GachaLetter
+//     {
+//         public string Letter;
+//         public Rarity rarity;
+//         
+//         public enum Rarity
+//         {
+//             
+//         }
+//     }
+//
+//     public class GetRandomGachaLetter
+//     {
+//         
+//     }
+//     
+//     private string GetRandomRarity()
+//     {
+//         float totalWeight = 0f;
+//
+//         foreach (float weight in rarityWeights.Values)
+//         {
+//             totalWeight += weight;
+//         }
+//
+//    
+//     }
+// }
